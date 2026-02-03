3413455c740f84355bedc1c60bd178faf8b7770c
sajrob <solomonroberts87@gmail.com>
2026-02-03 09:13:59 +0000

    changed the image seen when links are shared to the app logo

--- api/preview.ts
+++ api/preview.ts
@@ -16,8 +16,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     let title = 'My Wishlist';
     let description = 'Check out this wishlist on Wishlist App!';
-    let image = 'http://wishlist-eight-xi.vercel.app/og-default.png';
-    let url = `http://wishlist-eight-xi.vercel.app/share/${id}`;
+    let image = 'https://wishlist-eight-xi.vercel.app/hero-image.png';
+    let url = `https://wishlist-eight-xi.vercel.app/share/${id}`;
 
     if (supabase) {
         try {
